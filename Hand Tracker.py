f9a8e4670cac3d74d87a3019615e4744d3a5684a
mostafa mahmoud <mostafa_mahmoud@protonmail.com>
2015-12-30 12:18:43 +0200

    added hand detetion

--- Hand Tracker.py
+++ Hand Tracker.py
@@ -10,6 +10,7 @@ class HandTracker:
     DEBUGGING = True
     SPACE_KEY = 32
     ESC_KEY = 27
+    quit = False
     camera = None
     frame = None
     hand_row_point = None
@@ -19,25 +20,20 @@ class HandTracker:
     hand_histogram_captured = None
     hand_histogram = None
     skin_mask = None
+    contours = None
+    centroid = None
 
     def __init__(self):
         self.hand_histogram_captured = False
+        self.quit = False
 
     def start(self):
         self.start_camera()
-        while self.camera.isOpened():
+        while self.camera.isOpened() and not self.quit:
             if not self.hand_histogram_captured:
                 self.capture_hand_histogram()
             else:
-                # get feed from camera frame by frame
-                _, self.frame = self.camera.read()
-                self.apply_skin_mask(self.frame)
-                cv2.imshow('skin mask', self.skin_mask)
-                # Wait for a key press
-                pressed_key = cv2.waitKey(16)
-                if pressed_key == self.ESC_KEY:
-                    cv2.destroyAllWindows()
-                    break
+                self.detect_hand()
 
     def start_camera(self):
         self.camera = cv2.VideoCapture(0)
@@ -79,6 +75,9 @@ class HandTracker:
             self.scan_skin()
             cv2.destroyWindow('Cover all squares with your hand')
             self.hand_histogram_captured = True
+        elif pressed_key == self.ESC_KEY:
+            cv2.destroyAllWindows()
+            self.quit = True
 
     def scan_skin(self):
         # transform from rgb to hsv because it's easier
@@ -112,9 +111,89 @@ class HandTracker:
         cv2.GaussianBlur(self.skin_mask, (5, 5), 0, self.skin_mask)
         # remove faulty skin (kernel of size 9x9)
         cv2.morphologyEx(self.skin_mask, cv2.MORPH_OPEN, (31, 31), self.skin_mask, iterations=5)
-        # reduce black spaces in the hand
+        # reduce black holes in the hand
         cv2.morphologyEx(self.skin_mask, cv2.MORPH_CLOSE, (9, 9), self.skin_mask, iterations=5)
+        # Show skin detection result if DEBUGGING
+        if self.DEBUGGING:
+            cv2.imshow('SKIN', self.skin_mask)
         return self.skin_mask
+
+    def detect_hand(self):
+        assert self.camera.isOpened()
+        if not self.hand_histogram_captured:
+            return
+        # get feed from camera frame by frame
+        _, self.frame = self.camera.read()
+        self.locate_hand(self.frame)
+        # Draw hand convex hull on frame
+        cv2.drawContours(self.frame, self.contours, -1, [255, 255, 0], 3)
+        # Draw centroid as circle
+        cv2.circle(self.frame, self.centroid, 15, [0, 0, 255])
+        cv2.imshow('Hand Centroid', self.frame)
+        pressed_key = cv2.waitKey(16)
+        # If ESC is pressed destroy all OpenCV windows
+        if pressed_key == self.ESC_KEY:
+            self.quit = True
+            cv2.destroyAllWindows()
+
+    def locate_hand(self, frame):
+        # Mask the skin from the frame
+        self.skin_mask = self.apply_skin_mask(frame)
+        gray = cv2.cvtColor(self.skin_mask, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 50, 255, 0)
+        # detect counters with extreme outer flag
+        _, self.contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Apply filters on contours to locate hand
+        self.contours = self.filter_contours(frame, self.contours)
+        self.centroid = self.calc_centroid(self.contours[0])
+
+    @staticmethod
+    def filter_contours(frame, contours):
+        # remove faulty small contours detected by applying threshold on contour area
+        # First calc avg area of a contour
+        contour_area_sum = 0
+        for i in range(len(contours)):
+            contour_area_sum += cv2.contourArea(contours[i])
+        contour_avg_area = contour_area_sum / len(contours)
+        # Get the above average contours and not convex
+        possible_hand_contour = []
+        for i in range(len(contours)):
+            if cv2.contourArea(contours[i]) > contour_avg_area and not cv2.isContourConvex(contours[i]):
+                possible_hand_contour.append(contours[i])
+        # Now convert possible hand contours to convex
+        convex_hull = []
+        for y in range(len(possible_hand_contour)):
+            convex_hull.append(cv2.convexHull(possible_hand_contour[y]))
+
+        # # Now get mean color of every convex hull and return the closest to the peak of the hand histogram
+        # mean_colors = []
+        # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # for i in range(len(convex_hull)):
+        #     mask = np.zeros(hsv.shape[:2], np.uint8)
+        #     cv2.drawContours(mask, [convex_hull[i]], 0, 255, -1)
+        #     mean_colors.append(cv2.mean(hsv, mask=mask)[0])
+
+        # detect hand by find max area (not accurate technique)
+        max_area = 0
+        max_hull = None
+        for i in range(len(convex_hull)):
+            hull = convex_hull[i]
+            area = cv2.contourArea(hull)
+            if area > max_area:
+                max_area = area
+                max_hull = hull
+        return [max_hull]
+
+    @staticmethod
+    def calc_centroid(hand_contour):
+        m = cv2.moments(hand_contour)
+        cx = int(m['m10'] / m['m00'])
+        cy = int(m['m01'] / m['m00'])
+        return cx, cy
+
+    def get_centroid(self):
+        return self.centroid
+
 
 if __name__ == '__main__':
     HandTracker().start()
